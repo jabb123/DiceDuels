96713cb939a24c037e064fdc3f1a664ed18b1ac2
jabb123 <jabbinante@sjnma.org>
2017-03-06 17:08:36 -0600

    [initial commit to project]

---
+++ DiceDuels.py
@@ -0,0 +1,9 @@
+print("Dice Roller Game")
+print("two players roll the dice and see who can get higher number, best of three wins")
+from random import randrange 
+print(" Player1 first dice roll")
+Roll1= randrange(1,6)
+print(Roll1)
+print("Player2 first dice roll")
+Roll2= randrange(1,6)
+
