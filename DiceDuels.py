2899d36a9ba2e8843cda29edf97ded8c571af44f
jabb123 <jabbinante@sjnma.org>
2017-03-06 17:42:30 -0600

    [project restarted because i need to use loops... option for players to add names also included]

--- DiceDuels.py
+++ DiceDuels.py
@@ -1,22 +1,3 @@
-print("Dice Roller Game")
-print("two players roll the dice and see who can get higher number, best of three wins")
-from random import randrange 
-
-print("Player1 first dice roll")
-Roll1= randrange(1,6)
-print(Roll1)
-
-print("Player2 first dice roll")
-Roll2= randrange(1,6)
-print(Roll2)
-
-print("Player1 second dice roll")
-Roll3= randrange(1,6)
-print(Roll3)
-
-print("Player2 second dice roll")
-Roll4= randrange(1,6)
-print(Roll4)
-
-if(Roll1 > Roll2 and Roll3 > Roll4):
-	print("Player1 wins!")
+print("Welcome to dice duels")
+Player1= str(raw_input("Player1 please enter name- "))
+Player2= str(raw_input("Player2 please enter name- "))
