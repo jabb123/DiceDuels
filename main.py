0e68cf3a9163d56d089c6ffa835647b6f74544cb
jabb123 <jabbinante@sjnma.org>
2017-03-08 09:17:15 -0600

    [project done, still need to debug]

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ while(player1w!=2 and player2w!=2):
 	roll1= randrange(0,6)
 	print(roll1)
 	roll2= randrange(0,6)
-if (player1w>player2w):
-	player1 +=1
-	print("player one wins")
+	if (player1w>player2w):
+		player1w +=1
+		print("Player one wins the round!")
+	else (player2w>player1w):
+		player2w +=1
+		print("Player two wins the round!")
+	if player1w ==2:
+		print("Player one wins the game!")
+	else player2w ==2:
+		print("Player two wins the game!")
+
 
