230d8bd3c10f29e9e007056cc465169cf2dff6ea
jabb123 <jabbinante@sjnma.org>
2017-03-08 09:05:10 -0600

    [continuing loop cretions]

--- DiceDuels.py
+++
@@ -1,3 +0,0 @@
-print("Welcome to dice duels")
-Player1= str(raw_input("Player1 please enter name- "))
-Player2= str(raw_input("Player2 please enter name- "))
---
+++ main.py
@@ -0,0 +1,14 @@
+print("Welcome to dice duels")
+from random import randrange
+
+Player1w=0 
+player2w=0 
+player1w!=0 and player2w!=0 
+while(player1w!=2 and player2!=2):
+	raw_input("press enter to roll dice")
+	roll1= randrange(0,6)
+	print(roll1)
+	roll2= randrange(0,6)
+	if (player1w>player2w):
+	print(Player1 wins!)
+	player1
