1b6b0043c5e4b3aebeaf5a15ea11d2c1e14c4130
jabb123 <jabbinante@sjnma.org>
2017-03-09 10:38:32 -0600

    [Final commit, all bugs removed and program works fine]

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from random import randrange
 
 player1w =0 
 player2w =0 
-player1w!=0 and player2w!=0 
 while(player1w!=2 and player2w!=2):
 	raw_input("press enter to roll dice-")
-	roll1= randrange(0,6)
+	roll1= randrange(1,7)
 	print(roll1)
-	roll2= randrange(0,6)
+	roll2= randrange(1,7)
+	print(roll2)
 	if (roll1>roll2):
 		player1w +=1
 		print("Player one wins the round!")
-	elif(roll1 > roll2):
+	elif(roll1<roll2):
 		player2w +=1
 		print("Player two wins the round!")
-	if player1w ==2:
-		print("Player one wins the game!")
-	elif player2w ==2:
-		print("Player two wins the game!")
+if player1w ==2:
+	print("Player one wins the game!")
+elif player2w ==2:
+	print("Player two wins the game!")
 
 
