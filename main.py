e605c8ad19a414b12ef37c7386d9a38c6dc6f713
jabb123 <jabbinante@sjnma.org>
2017-04-05 10:57:22 -0500

    [changes made to original diceduels]

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 print("Welcome to dice duels")
 from random import randrange
 
+player1=str(raw_input("player 1 enter name-"))
+player2=str(raw_input("player 2 enter name-"))
+
 player1w =0 
 player2w =0 
-while(player1w!=2 and player2w!=2):
+while(player1w!=3 and player2w!=3):
 	raw_input("press enter to roll dice-")
 	roll1= randrange(1,7)
 	print(roll1)
@@ -11,13 +14,11 @@ while(player1w!=2 and player2w!=2):
 	print(roll2)
 	if (roll1>roll2):
 		player1w +=1
-		print("Player one wins the round!")
+		print(player1 + " wins the round!")
 	elif(roll1<roll2):
 		player2w +=1
-		print("Player two wins the round!")
-if player1w ==2:
-	print("Player one wins the game!")
-elif player2w ==2:
-	print("Player two wins the game!")
-
-
+		print(player2 + " wins the round!")
+if player1w ==3:
+	print(player1 + " wins the game!")
+if player2w ==3:
+	print(player2 + " wins the game!")
